5dfb871306da2df306f5fc13d4ce34735c23d46e
leonardolopesb <leonardolopbraga@gmail.com>
2025-07-31 12:02:01 -0300

    Criação de rotas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,17 +6,11 @@ const App = () => {
   return (
     <div>
       <Header />
+
       <main>
         <Routes />
-
-        <p>Welcome to your mobile-first React app!</p>
-
-        <p>This is a simple example of a React application.</p>
-
-        <p>Feel free to explore and modify the code.</p>
-
-        <p>Enjoy building with React!</p>
       </main>
+
       <Footer />
     </div>
   );
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom"
+import { BrowserRouter, Navigate, Routes as RouterRoutes, Route } from "react-router-dom"
+import { Main } from "../pages/MainPage"
+import { Dashboard } from "../pages/Dashboard";
 
 export const Routes = () => {
-
     return (
         <BrowserRouter>
             <RouterRoutes>
-                <Route path="/" element={<h1>Olá</h1>} />
+                <Route path="/" element={<Main />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+
+                <Route path="*" element={<Navigate to="/" />} />
             </RouterRoutes>
         </BrowserRouter>
     )
