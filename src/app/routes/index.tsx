94b9699c4200108d90179f58d8174c165e6394d7
leonardolopesb <leonardolopbraga@gmail.com>
2025-08-01 15:33:09 -0300

    'Login' no lugar de 'Dashboard'

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Navigate, Routes as RouterRoutes, Route } from "react-router-dom"
 import { Main } from "../pages/tsx/MainPage"
-import { Dashboard } from "../pages/tsx/Dashboard";
+import { LoginPage } from "../pages/tsx/LoginPage";
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <RouterRoutes>
                 <Route path="/" element={<Main />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/login" element={<LoginPage />} />
 
                 <Route path="*" element={<Navigate to="/" />} />
             </RouterRoutes>
